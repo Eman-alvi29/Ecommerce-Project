077845723a30251e0605838a1fbf220df7a2259b
Eman-alvi29 <ez130529@gmail.com>
2024-08-02 11:24:44 +0500

    Added Products on Homepage

--- Ecommerce/app/admin/items.rb
+++ Ecommerce/app/admin/items.rb
@@ -1,42 +1,51 @@
 ActiveAdmin.register Item do
-  permit_params :name, :description, :price, :category_id, size_ids: []
-
+  permit_params :name, :description, :price, :category_id, :image, size_ids: []
   index do
     selectable_column
     id_column
     column :name
     column :description
     column :price
-    column :category do |item|
-      item.category.name.humanize if item.category
-    end
+    column :category
     column :sizes do |item|
       item.sizes.map(&:name).join(', ')
     end
+    column :image do |item|
+      if item.image.attached?
+        image_tag url_for(item.image), size: "50x50"
+      end
+    end
     actions
   end
-
+  filter :name
+  filter :description
+  filter :price
+  filter :category
+  filter :sizes, as: :select, collection: -> { Size.all.map { |s| [s.name, s.id] } }
   form do |f|
     f.inputs do
       f.input :name
       f.input :description
       f.input :price
-      f.input :category_id, as: :select, collection: Category.names.keys.collect { |name| [name.humanize, name] }
+      f.input :category
       f.input :sizes, as: :check_boxes, collection: Size.all.map { |s| [s.name, s.id] }
+      f.input :image, as: :file
     end
     f.actions
   end
-
   show do
     attributes_table do
       row :name
       row :description
       row :price
-      row :category do |item|
-        item.category.name.humanize if item.category
-      end
+      row :category
       row :sizes do |item|
         item.sizes.map(&:name).join(', ')
+      end
+      row :image do |item|
+        if item.image.attached?
+          image_tag url_for(item.image), size: "50x50"
+        end
       end
     end
   end
--- Ecommerce/config/routes.rb
+++ Ecommerce/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  #devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  root 'welcome#index'
+
+  get 'men_fashion', to: 'pages#men_fashion'
+  get 'women_fashion', to: 'pages#women_fashion', as: 'women_fashion'
+  get 'all_fashion', to: 'pages#all_fashion', as: 'all_fashion'
+
+  # Root route
+  root 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
