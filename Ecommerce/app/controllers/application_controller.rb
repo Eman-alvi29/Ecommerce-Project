555ddaf0cf2ed699daf56f28dd1c54170b644485
Eman-alvi29 <ez130529@gmail.com>
2024-08-06 16:07:54 +0500

    Feedback

--- Ecommerce/app/controllers/application_controller.rb
+++ Ecommerce/app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path # Redirect to the login page
   end
-   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+
    allow_browser versions: :modern
 end
--- Ecommerce/app/models/item_size.rb
+++ Ecommerce/app/models/item_size.rb
@@ -1,5 +1,6 @@
 class ItemSize < ApplicationRecord
   include Ransackable
+
   belongs_to :item
   belongs_to :size
 end
