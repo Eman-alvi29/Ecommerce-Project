f9111956d1674ae0aefbc37d6f85d63590330dc0
Eman-alvi29 <ez130529@gmail.com>
2024-08-06 15:35:50 +0500

    Feedback

--- Ecommerce/app/controllers/categories_controller.rb
+++ Ecommerce/app/controllers/categories_controller.rb
@@ -1,15 +1,16 @@
 class CategoriesController < ApplicationController
-  def men_fashion
-    @category = Category.find_by(name: 'Male')
-    @items = @category ? @category.items : []
+  def index
+    @category = params[:category]
+    @items = if @category.present?
+               Item.where(category: @category)
+             else
+               Item.all
+             end
   end
 
-  def women_fashion
-    @category = Category.find_by(name: 'Female')
-    @items = @category ? @category.items : []
-  end
+  def show
+    @category = params[:id]
+    @items = Item.where(category: @category)
 
-  def all_fashion
-    @items = Item.all
   end
 end
--- Ecommerce/config/routes.rb
+++ Ecommerce/config/routes.rb
@@ -2,16 +2,12 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-
-  get 'categories/men_fashion', to: 'categories#men_fashion'
-  get 'categories/women_fashion', to: 'categories#women_fashion'
-  get 'categories/all_fashion', to: 'categories#all_fashion'
-
+  root 'welcome#index'
+  resources :categories, only: [:index, :show]
+  get 'categories/:category', to: 'categories#show', as: 'category_items'
+  #  get 'categories', to: 'categories#index', as: 'categories'
+  #  get 'categories/:id', to: 'categories#show', as: 'category'
   # Root route
-  root 'categories#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
